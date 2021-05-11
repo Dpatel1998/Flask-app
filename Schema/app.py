c687ea49ff00f892fa15b59b6c840a033c50c163
Your Name <you@example.com>
2021-05-11 16:47:07 +0100

    End of day

--- Schema/app.py
+++ Schema/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy class
 
 app = Flask(__name__) # create Flask object
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://database:password@34.89.110.105:3306/sakila' # Set the connection string to connect to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@34.89.110.105:3306/sakila' # Set the connection string to connect to the database
 
 db = SQLAlchemy(app)
 class Users(db.Model):
